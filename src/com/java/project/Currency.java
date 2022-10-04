bc9a99967b38263b85ae09631cfdcdab92471e4c
seoyun <may-primavera@naver.com>
2022-10-04 14:26:41 +0900

    feat: 화폐별 개수 출력

--- src/com/java/project/Currency.java
+++ src/com/java/project/Currency.java
@@ -3,22 +3,21 @@ package com.java.project;
 public class Currency {
     public static void main(String[] args) {
         //한화로 구하는 것
-        int[] money = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
+        //raw한 코드로 접근
 
         int curr50000 = 50000;
         int curr10000 = 10000;
         int curr5000 = 5000;
         int curr1000 = 1000;
-        int curr100 = 100;
+        int curr500 = 500;
 
-        //raw한 코드로 접근
         int refund = 25000;
 
-        System.out.printf("5만원권 %d장\n", refund / curr50000); //25000원을 5만원으로 거슬러주면? 0장
-        System.out.printf("1만원권 %d장\n", refund / curr10000); //25000원을 5만원으로 거슬러주면? 0장
-        System.out.printf("5천원권 %d장\n", refund / curr5000); //25000원을 5만원으로 거슬러주면? 0장
-        System.out.printf("1천원권 %d장\n", refund / curr1000); //25000원을 5만원으로 거슬러주면? 0장
-        System.out.printf("백원권 %d장\n", refund / curr100); //25000원을 5만원으로 거슬러주면? 0장
+        System.out.printf("5만원권 몫:%d장 나머지:%d\n", refund / curr50000, refund % curr50000);
+        System.out.printf("1만원권 몫:%d장 나머지:%d\n", refund / curr10000, refund % curr10000);
+        System.out.printf("5천원권 몫:%d장 나머지:%d\n", refund / curr5000, refund % curr5000);
+        System.out.printf("1천원권 몫:%d장 나머지:%d\n", refund / curr1000, refund % curr1000);
+        System.out.printf("백원권 몫:%d장 나머지:%d\n", refund / curr500, refund % curr500);
 
     }
 }
