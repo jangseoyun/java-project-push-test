847548725eadcd8be1e47c486d714d2634528506
seoyun <may-primavera@naver.com>
2022-10-04 14:22:18 +0900

    commit: 주석 추가

--- src/com/java/project/CurrencyCnt.java
+++ src/com/java/project/CurrencyCnt.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class CurrencyCnt {
     public static void main(String[] args) {
+        //화폐 매수를 구하는 프로그램 구현
+        //각 화폐 단위로 나누었을 때 몫과 나머지를 구하는 문제
+        //나머지를 변수에 누적
         int[] money = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
 
         Scanner sc = new Scanner(System.in);
